8168ea6005acc5930d2ab7b075daa89cca07b0e9
Daniel Bartolomé <dbartolo@bancolombia.com.co>
2020-01-08 13:41:04 -0500

    Ejercicio 1. Primeros cambios let y nombre var.

---
+++ ejercicio1/appFinal.ts
@@ -0,0 +1,32 @@
+// Tipos
+let batman1 = "Bruce";
+let superman1 = "Clark";
+
+let existe1 = false;
+
+// Tuplas
+let parejaHeroes1 = [batman1,superman1];
+let villano1 = ["Lex Lutor",5,true];
+
+// Arreglos
+let aliados1 = ["Mujer Maravilla","Acuaman","San", "Flash"];
+
+//Enumeraciones
+let fuerzaFlash1 = 5;
+let fuerzaSuperman1 = 100;
+let fuerzaBatman1 = 1;
+let fuerzaAcuaman1 = 0;
+
+// Retorno de funciones
+function actilet_batiseñal(){
+  return "activada";
+}
+
+function pedir_ayuda(){
+  console.log("Auxilio!!!");
+}
+
+// Aserciones de Tipo
+let poder1 = "100";
+let largoDelPoder1 = poder.length;
+console.log( largoDelPoder1 );
