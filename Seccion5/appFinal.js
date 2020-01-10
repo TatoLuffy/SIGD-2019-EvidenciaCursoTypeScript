60acb33a2e83eee542a0f737af81bc57236c8fb1
Daniel Bartolomé <dbartolo@bancolombia.com.co>
2020-01-10 18:49:13 -0500

    Seccion 5. Transpilar files auto tsconfig.json

---
+++ Seccion5/appFinal.js
@@ -0,0 +1,26 @@
+"use strict";
+// Tipos
+var batman2 = "Bruce";
+var superman2 = "Clark";
+var existe2 = false;
+// Tuplas
+var parejaHeroes2 = [batman2, superman2];
+var villano2 = ["Lex Lutor", 5, true];
+// Arreglos
+var aliados2 = ["Mujer Maravilla", "Acuaman", "San", "Flash"];
+//Enumeraciones
+var fuerzaFlash2 = 5;
+var fuerzaSuperman2 = 200;
+var fuerzaBatman2 = 2;
+var fuerzaAcuaman2 = 0;
+// Retorno de funciones
+function actilet_batiseñal2() {
+    return "activada";
+}
+function pedir_ayuda2() {
+    console.log("Auxilio!!!");
+}
+// Aserciones de Tipo
+var poder2 = "200";
+var largoDelPoder2 = poder2.length;
+console.log(largoDelPoder2);
---
+++ Seccion5/appFinal.ts
@@ -0,0 +1,32 @@
+// Tipos
+let batman2:string = "Bruce";
+let superman2:string = "Clark";
+
+let existe2:boolean = false;
+
+// Tuplas
+let parejaHeroes2:[string,string] = [batman2,superman2];
+let villano2:[string,number,boolean] = ["Lex Lutor",5,true];
+
+// Arreglos
+let aliados2:string[] = ["Mujer Maravilla","Acuaman","San", "Flash"];
+
+//Enumeraciones
+let fuerzaFlash2:number = 5;
+let fuerzaSuperman2:number = 200;
+let fuerzaBatman2:number = 2;
+let fuerzaAcuaman2:number = 0;
+
+// Retorno de funciones
+function actilet_batiseñal2():string{
+  return "activada";
+}
+
+function pedir_ayuda2():void{
+  console.log("Auxilio!!!");
+}
+
+// Aserciones de Tipo
+let poder2:string = "200";
+let largoDelPoder2:number = poder2.length;
+console.log( largoDelPoder2 );
